ed18aa78016248b0a70c579e4d6990806a560061
Daria Veselkova <daria.veselkova@yandex.ru>
2021-12-29 23:46:08 +0300

    Added a template for individuals

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    page_title = "Электронный каталог хранения"
-    return render_template('hello.html', title=page_title)
+#def hello():
+#    page_title = "Электронный каталог хранения"
+#    return render_template('hello.html', title=page_title)
+
+
+#@app.route('/api/v1/individuals/')
+def show_individuals():
+    response = requests.get('http://localhost:5000/api/v1/individuals/')
+    response.raise_for_status()
+    individuals = response.json
+    return render_template('individuals.html', individuals=individuals)
