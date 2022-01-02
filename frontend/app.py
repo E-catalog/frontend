360cce5d2db9af85a7629b9861cbb520600cc2b3
Daria Veselkova <daria.veselkova@yandex.ru>
2022-01-02 13:16:12 +0300

    Added config.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests
+import frontend.config as config
 
 
 app = Flask(__name__)
@@ -8,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def show_individuals():
     title = "Электронный каталог хранения"
-    response = requests.get('http://192.168.1.64:5000/api/v1/individuals/')
+    response = requests.get(config.API_URL)
     response.raise_for_status()
     individuals = response.json()
     return render_template('individuals.html', title=title, individuals=individuals)
