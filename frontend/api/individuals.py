9ee630004699f31b31e5052c4fb65523a9af940d
Daria Veselkova <daria.veselkova@yandex.ru>
2022-01-12 22:43:42 +0300

    feature: added update form, functions to get and update ind, placed update form into modal

--- frontend/api/individuals.py
+++ frontend/api/individuals.py
@@ -16,6 +16,12 @@ class IndividualsClient:
         data = response.json()
         return [Individual(**item) for item in data]
 
+    def get(self, uid: int) -> Individual:
+        response = httpx.get(f'{self.url}/{uid}')
+        response.raise_for_status()
+        data = response.json()
+        return Individual(**data)
+
     def add(self, payload: Individual) -> int:
         new_individual = payload.dict()
         response = httpx.post(f'{self.url}/', json=new_individual)
--- frontend/app.py
+++ frontend/app.py
@@ -14,14 +14,26 @@ client = Client(config.api_url)
 
 
 @app.route('/')
-def show_individuals():
+def show_individuals(individual=None):
     title = 'Электронный каталог хранения'
     individuals = client.individuals.get_all()
     return render_template(
         'individuals.html',
         title=title,
         individuals=[item.dict() for item in individuals],
+        individual=individual,
     )
+
+
+@app.route('/individual')
+def get_individual():
+    form_data: dict[str, Any] = request.args
+    if not form_data:
+        abort(HTTPStatus.BAD_REQUEST, 'Отсутствуют данные')
+
+    id_from_form = form_data['id']
+    individual = client.individuals.get(id_from_form)
+    return show_individuals(individual.dict())
 
 
 @app.route('/', methods=['POST'])
@@ -39,3 +51,8 @@ def add_individual():
 
     client.individuals.add(payload)
     return redirect(url_for('show_individuals'))
+
+
+@app.route('/', methods=['PUT'])
+def update_individual():
+    pass
