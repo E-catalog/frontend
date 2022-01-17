d87a3c6d4e537b690e4cabe281643e73b9bdcf06
Daria Veselkova <daria.veselkova@yandex.ru>
2022-01-17 12:49:27 +0300

    chore: updated pydantic model for ind, added model for place

--- frontend/api/models.py
+++ frontend/api/models.py
@@ -1,17 +1,27 @@
 from typing import Optional
 
 from pydantic import BaseModel
-from pydantic.fields import Field
 
 
-class Individual(BaseModel):
-    uid: int = Field(alias='id')
-    place: str
+class Model(BaseModel):
+    uid: int
+
+
+class Individual(Model):
     name: str
+    place_uid: int
     year_of_excavation: Optional[int]
     sex: Optional[str]
     age: Optional[str]
     individual_type: Optional[str]
     preservation: Optional[str]
     epoch: Optional[str]
+    comments: Optional[str]
+
+
+class Place(Model):
+    name: str
+    head_of_excavations: Optional[str]
+    type_of_burial_site: Optional[str]
+    coordinates: Optional[str]
     comments: Optional[str]
